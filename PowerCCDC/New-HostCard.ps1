1cdf21ec7ee162711dc179ffaff5e62a42be5627
Justin <jcovairt01@gmail.com>
2022-09-21 00:14:41 -0700

    lasdaweqe

--- PowerCCDC/New-HostCard.ps1
+++ PowerCCDC/New-HostCard.ps1
@@ -1,4 +1,4 @@
-function Invoke-Inventory {
+function New-HostCard {
     param (
         [Parameter(Mandatory)]
         [String] $BoardName,
@@ -23,15 +23,16 @@ function Invoke-Inventory {
             }
             $password = Read-Host "Enter a new password"
             $CardTitle = -join($hostname," (",$ip,") ",'[', $User,"] ") 
-            $BoxCard = New-TrelloCard -ListID (Get-TrelloList -BoardId (Get-TrelloBoard -Name $BoardName | Select-Object -ExpandProperty id) | Select-Object name,id | Where-Object name -eq $OS | Select-Object -expand id) -Name $CardTitle -Description "# System Information:
-## Operating System:
+            $description = "# System Information:
+ ## Operating System:
 
-## Admin User
+ ## Admin User
 username: $(whoami)
 password: $password
 
 ## Other Details:
     Here"
+            $BoxCard = New-TrelloCard -ListID (Get-TrelloList -BoardId (Get-TrelloBoard -Name $BoardName | Select-Object -ExpandProperty id) | Select-Object name,id | Where-Object name -eq $OS | Select-Object -expand id) -Name $CardTitle -Description $description
             New-TrelloCardChecklist -Card $BoxCard -Name Baselining -Item @('Inventory', 'Change Default Passwords', 'Configure Log Forwarding')
             New-TrelloCardChecklist -Card $BoxCard -name 'Password' -Item $password
     }
