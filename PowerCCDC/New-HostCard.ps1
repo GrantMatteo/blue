dfde805e8f0fef6e21bbd996b8f9fc4f53691c40
justin <jcovairt01@gmail.com>
2022-09-22 14:25:55 -0700

    asdaasdasdas

--- PowerCCDC/New-HostCard.ps1
+++ PowerCCDC/New-HostCard.ps1
@@ -19,23 +19,47 @@ function New-HostCard {
 # System Information
 
 ## Operating System:
-PlaceHolder
+[OS]
 
 ## Admin User
 username: $(whoami)
 password: $password
 
 ## Other Details:
-    Here"
+    DNS Servers: [DNS]"
             if ($System -eq 'Linux'){
                 $IP = hostname -I
+                $DNS = cat /etc/resolv.conf | grep nameserver | awk '{print $2}'
                 $OperatingSystem = cat /etc/os-release  | grep PRETTY_NAME | sed 's/PRETTY_NAME=//' | sed 's/\"//g'
-                $description = $description -Replace "PlaceHolder", $OperatingSystem
+                $description = $description -Replace "[OS]", $OperatingSystem
+                $description = $description -Replace "[DNS]", $DNS
                 $CardTitle = $CardTitle -Replace "IP", $IP
                 $BoxCard = New-TrelloCard -ListID (Get-TrelloList -BoardId (Get-TrelloBoard -Name $Board | Select-Object -ExpandProperty id) | Select-Object name,id | Where-Object name -eq Linux | Select-Object -expand id) -Name $CardTitle -Description $description
-                
-                # Add comment with output of inventory script
-
+                $userchecklist = New-TrelloCardChecklist -Card $BoxCard -Name users
+                $servicechecklist = New-TrelloCardChecklist -Card $BoxCard -Name services
+                $inboundchecklist = New-TrelloCardChecklist -Card $BoxCard -Name inbound
+                $outboundchecklist = New-TrelloCardChecklist -Card $BoxCard -Name outbound
+                $users = cat /etc/passwd | grep -vE 'false|nologin|sync' | awk -F ":" '{print $1}'
+                $outbound = netstat -tupwn | grep -E 'tcp|udp' | awk '{print $5,$7}'
+                $inbound = netstat -tulpen | grep -E 'tcp|udp' | awk '{print $4,$9}'
+                foreach ($user in $users){
+                    New-TrelloCardChecklistItem -Checklist $userchecklist -Name $user
+                }
+                if ($null -eq (which systemctl)){
+                    $services = systemctl --type=service  | grep active | awk '{print $1}'
+                }
+                else{
+                    $services = service --status-all | grep -i '+' | awk -F "]  " '{print $2}'
+                }
+                foreach ($service in $services){
+                    New-TrelloCardChecklistItem -Checklist $servicechecklist -Name $service
+                }
+                foreach ($connection in $outbound){
+                    New-TrelloCardChecklistItem -Checklist $outboundchecklist -Name $connection
+                }
+                foreach ($connection in $inbound){
+                    New-TrelloCardChecklistItem -Checklist $inboundchecklist -Name $connection
+                }
             }
             elseif ($System -eq 'Windows') {
                 $IP = Get-NetIPAddress | Where-Object AddressFamily -eq 'IPv4' | Select-Object IPAddress | Where-Object IPAddress -NotLike '127.0.0.1' | Select-Object -ExpandProperty IPAddress
