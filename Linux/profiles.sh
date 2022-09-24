2c258e2d9588f587855dfb6218c21f0fce74a45d
justin <jcovairt01@gmail.com>
2022-09-24 16:32:26 -0700

    bruh

--- Linux/profiles.sh
+++ Linux/profiles.sh
@@ -5,5 +5,5 @@ mkdir /opt/quarentine
 mv /etc/prof{i,y}le* /opt/quarentine
 
 for file in '.profile' '.bashrc' '.bash_login' '.zshrc'; do
-    find /home /root -name "$file" -exec rm {};;
+    find /home /root -name "$file" -exec rm {}\;
 done
