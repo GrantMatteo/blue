263b760926bfd314cf437d3bb3879f2cc1602a23
justin <jcovairt01@gmail.com>
2022-09-26 19:16:32 -0700

    removed profiles

--- Linux/profiles.sh
+++
@@ -1,9 +0,0 @@
-# Removes system profiles and bashrc
-
-mkdir /opt/quarentine
-
-mv /etc/prof{i,y}le* /opt/quarentine
-
-for file in '.profile' '.bashrc' '.bash_login' '.zshrc'; do
-    find /home /root -name "$file" -exec rm {}\;
-done
