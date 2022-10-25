6033e35b796bf9e2dc09acf6fd3eccc61fa6a1ad
evanjd711 <evanjd711@gmail.com>
2022-10-24 17:29:06 -0700

    Firewall Rule Menace

---
+++ Windows/FirewallInboundTemplate.ps1
@@ -0,0 +1,28 @@
+$PortToApp = Get-NetTCPConnection | Select-Object -Property LocalPort,@{name='ProcessName';expression={(Get-Process -Id $_.OwningProcess). Path}},CreationTime
+
+$PortTable = @{}
+
+$PortToApp | ForEach-Object {$PortTable.Add($_.LocalPort, $_.ProcessName)}
+
+Get-NetFirewallRule | Remove-NetFirewallRule
+
+Set-NetFirewallProfile -All -LogBlocked True -LogMaxSizeKilobytes 16384 -LogAllowed False -LogFileName "%systemroot%\System32\LogFiles\Firewall\pfirewall.log"
+
+Set-NetFirewallProfile -All -DefaultInboundAction Block -DefaultOutboundAction Block -Enabled True
+
+netsh a s a state off
+
+netsh a s a state on
+
+Start-Sleep -Seconds 45
+
+$LogIn = Get-Content "$Env:SystemRoot\System32\LogFiles\Firewall\pfirewall.log" | Select-String -Pattern "DROP TCP" | Select-String -Pattern "RECEIVE"
+
+
+for ($i = 0; $i -lt $LogIn.Length; $i++) {
+    $LogIn[$i] = $LogIn[$i].ToString().Split(" ")[5] + ":" + $LogIn[$i].ToString().Split(" ")[7]
+}
+
+$SocketsIn = $LogIn | Select-Object -Unique
+
+$SocketsIn | ForEach-Object {$Program = $_.split(":")[1]; New-NetFirewallRule -DisplayName "$_" -Name "$_" -Program ($PortTable.GetEnumerator() | Where-Object key -eq $Program | Select-Object -expand Value) -Direction Inbound -Action Allow -LocalPort $_.split(":")[1] -Protocol TCP -Enabled True -Profile Any}
--- Windows/FirewallOutboundTemplate.ps1
+++ Windows/FirewallOutboundTemplate.ps1
@@ -10,13 +10,12 @@ netsh a s a state on
 
 Start-Sleep -Seconds 45
 
-$LogOut = Get-Content "$Env:SystemRoot\System32\LogFiles\Firewall\pfirewall.log" | Select-String -Pattern "DROP TCP"
-
 for ($i = 0; $i -lt $LogOut.Length; $i++) {
     $LogOut[$i] = $LogOut[$i].ToString().Split(" ")[5] + ":" + $LogOut[$i].ToString().Split(" ")[7]
 }
 
-$Sockets = $LogOut | Select-Object -Unique
+$LogOut = Get-Content "$Env:SystemRoot\System32\LogFiles\Firewall\pfirewall.log" | Select-String -Pattern "DROP TCP" | Select-String -Pattern "SEND"
 
-$Sockets | ForEach-Object {New-NetFirewallRule -DisplayName "$_" -Direction Inbound -Action Allow -LocalPort $_.split(":")[1] -Protocol TCP -Enabled True -Profile Any}
- 
+$SocketsOut = $LogOut | Select-Object -Unique
+
+$SocketsOut | ForEach-Object {New-NetFirewallRule -DisplayName "$_" -Direction Outbound -Action Allow -LocalPort $_.split(":")[1] -Protocol TCP -Enabled True -Profile Any}
