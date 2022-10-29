94b56b28009c11977db31a8bdb16ed5a11436e50
justin <jcovairt01@gmail.com>
2022-10-29 15:49:17 -0700

    more logging :D

--- Threat Hunting/Syslog/forwardersetup.sh
+++ Threat Hunting/Syslog/forwardersetup.sh
@@ -14,6 +14,8 @@ else
 fi
 
 cat << EOF > /etc/rsyslog.conf
+
+# Ubuntu Auth 
 \$ModLoad imfile 
 \$InputFileName /var/log/auth.log 
 \$InputFileStateFile auth_log 
@@ -22,18 +24,44 @@ cat << EOF > /etc/rsyslog.conf
 \$InputFileFacility local1
 \$InputRunFileMonitor
 
+# CentOS Auth
+\$InputFileName /var/log/secure
+\$InputFileStateFile auth_log
+\$InputFileTag auth_log
+\$InputFileSeverity info
+\$InputFileFacility local1
+\$InputFileRunMonitor
+
+# Ubuntu Apache2
 \$InputFileName /var/log/apache2/access.log
 \$InputFileStateFile access_log
 \$InputFileTag access_log
 \$InputFileSeverity info
-\$InputFileFacility local3
+\$InputFileFacility local2
 \$InputFileRunMonitor
 
+# RHEL Apache2
+\$InputFileName /var/log/httpd/access_log
+\$InputFileStateFile access_log
+\$InputFileTag access_log
+\$InputFileSeverity info
+\$InputFileFacility local2
+\$InputFileRunMonitor
+
+# Nginx
+\$InputFileName /var/log/nginx/access.log
+\$InputFileStateFile access_log
+\$InputFileTag access_log
+\$InputFileSeverity info
+\$InputFileFacility local2
+\$InputFileRunMonitor
+
+# Honeypot logging (thepot.sh)
 \$InputFileName /var/log/honeypot
 \$InputFileStateFile honeypot
 \$InputFileTag honeypot
 \$InputFileSeverity info
-\$InputFileFacility local4
+\$InputFileFacility local3
 \$InputFileRunMonitor
 
 *.* @$IP:514
