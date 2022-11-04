e12de4544450586b580939741fab686f478b66e6
susMdT <68256613+susMdT@users.noreply.github.com>
2022-11-03 19:58:01 -0700

    Update password.sh

--- Linux/password.sh
+++ Linux/password.sh
@@ -2,8 +2,8 @@
 echo "username,password" > $(hostname).csv
 for user in $(cat /etc/passwd | grep -P "/bin/.*sh" | cut -f1 -d':'); do
         pass=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1)
-        #echo $user:$pass | chpasswd
+        echo $user:$pass | chpasswd
         echo "$user,$pass" >> $(hostname).csv
 done
-#rm $(hostname).csv
-#rm $(basename "$0")
+cat $(hostname).csv
+rm $(hostname).csv
