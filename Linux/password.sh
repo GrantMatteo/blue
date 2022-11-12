5c2bf703ebd480e27333eb3c4bd4462c47bc263e
susMdT <68256613+susMdT@users.noreply.github.com>
2022-11-11 22:45:19 -0800

    Update password.sh
    bro i forgot numbers

--- Linux/password.sh
+++ Linux/password.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "username,password" > $(hostname).csv
 for user in $(cat /etc/passwd | grep -P "/bin/.*sh" | cut -f1 -d':'); do
-        pass=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1)
+        pass=$(cat /dev/urandom | tr -dc '[:alpha:][:digit:]' | fold -w ${1:-20} | head -n 1)
         echo $user:$pass | chpasswd
         echo "$user,$pass" >> $(hostname).csv
 done
