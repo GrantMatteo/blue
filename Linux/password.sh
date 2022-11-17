a2e75d86ffaac168bb6982f54ad97487a40cf7e7
susMdT <68256613+susMdT@users.noreply.github.com>
2022-11-17 15:33:15 -0800

    sudoeruser and root pass

--- Linux/password.sh
+++ Linux/password.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 echo "username,password" > $(hostname).csv
-for user in $(cat /etc/passwd | grep -P "/bin/.*sh" | cut -f1 -d':'); do
+for user in $(cat /etc/passwd | grep -E "/bin/.*sh" | cut -f1 -d':'); do
         pass=$(cat /dev/urandom | tr -dc '[:alpha:][:digit:]' | fold -w ${1:-20} | head -n 1)
-        echo $user:$pass | chpasswd
-        echo "$user,$pass" >> $(hostname).csv
+	if [ $user = "root" ]; then
+                echo $user:$ROOTPASS | chpasswd
+                echo "$user,$ROOTPASS" >> $(hostname).csv
+	elif [ $user = $USER ]; then
+		echo $USER:$PASS | chpasswd
+		echo "$USER,$PASS" >> $(hostname).csv
+	else
+        	echo $user:$pass | chpasswd
+		echo "$user,$pass" >> $(hostname).csv
+        fi
 done
 cat $(hostname).csv
 rm $(hostname).csv
