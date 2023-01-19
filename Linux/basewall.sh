971b948cf82ef71ca1779cae2fa802d0d2b36616
susMdT <68256613+susMdT@users.noreply.github.com>
2023-01-18 16:33:09 -0800

    Update basewall.sh

--- Linux/basewall.sh
+++ Linux/basewall.sh
@@ -12,8 +12,9 @@ $ipt -F; $ipt -X ;$ipt -P INPUT ACCEPT ; $ipt -P OUTPUT ACCEPT ; $ipt -P FORWARD
 # Allow our machine to respond to connections
 $ipt -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-# Allow outbound connetions to dependencies we need from other machines
+# Allow outbound connetions to dependencies we need from other machines + outbound local network DNS
 $ipt -A OUTPUT -d 127.0.0.1,$LOCALNETWORK -m conntrack --ctstate NEW -j ACCEPT
+$ipt -A OUTPUT -p udp --dport 53 -m conntrack -s 127.0.0.1,$LOCALNETWORK -j ACCEPT
 
 # Drop Output, but still allow new inbound. Allow forward for docker
 $ipt -P FORWARD ACCEPT; $ipt -P OUTPUT DROP;
