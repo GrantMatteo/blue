8537a11e6ea1238c642629c459158d05c6296b31
Altoid0 <tanay.v.shah@gmail.com>
2023-01-19 16:30:50 -0800

    winrm error checking and manual intervention

--- Windows/Invoke-Backup.ps1
+++ Windows/Invoke-Backup.ps1
@@ -1,27 +1,41 @@
+$ErrorActionPreference = "SilentlyContinue" # COMMENT IF U WANT TO SEE ERRORS
 $Computers = Get-ADComputer -filter * -Properties * | Where-Object OperatingSystem -Like "*Windows*" | Select-Object -ExpandProperty DNSHostname
 $Denied = @()
 $Sessions = @()
 foreach ($Computer in $Computers) {
-    try {
-        $TestSession = New-PSSession -ComputerName $Computer
+    $TestSession = New-PSSession -ComputerName $Computer
+    if ($TestSession) {
         $Sessions += $TestSession
+        Write-Host "[INFO] Preparing to WinRM to: $Computer" -ForegroundColor Green
     }
-    catch {
+    else {
         $Denied += $Computer
         Write-Host "[ERROR] Failed: $Computer" -ForegroundColor Red
     }
 }
 
-foreach ($Session in $Sessions) {
-    Read-Host -Prompt "Continue with invetory? Press Enter to continue."
-    $Inventory = Invoke-Command -FilePath $env:ProgramFiles\blue\windows\TrelloAutomation\TrelloAutomation.ps1 -Session $Session -AsJob
-    Wait-Job $Inventory
-    $r = Receive-Job $Inventory
-    $r | ForEach-Object {$_ > $env:ProgramFiles\blue\windows\logs\$($_.PSComputerName).trlout}
-
-    Read-Host -Prompt "Continue with Hardening? Press Enter to continue."
-    $Hardening = Invoke-Command -FilePath $env:ProgramFiles\blue\windows\Invoke-SecureBaseline.ps1 -Session $Session -AsJob
-    Wait-Job $Hardening
-    $r = Receive-Job $Hardening
-    $r | ForEach-Object {$_ > $env:ProgramFiles\blue\windows\logs\$($_.PSComputerName).secbout}
+if ($Denied.Count -gt 0) {
+    $Ans = Read-Host -Prompt "Not All computers have WinRM enabled, are you sure you want to continue? [y/n]"
+    while ($Ans -ne "y" -and $Ans -ne "n") {
+        $Ans = Read-Host -Prompt "Not All computers have WinRM enabled, are you sure you want to continue? [y/n]"
+    }
+}
+if ($Ans -eq "n") {
+    Write-Host "[INFO] Exiting..." -ForegroundColor Yellow
+    exit
+}
+if ($Ans -eq "y" -or $Denied.Count -eq 0) {
+    foreach ($Session in $Sessions) {
+        Read-Host -Prompt "Continue with invetory? Press Enter to continue."
+        $Inventory = Invoke-Command -FilePath $env:ProgramFiles\blue\windows\TrelloAutomation\TrelloAutomation.ps1 -Session $Session -AsJob
+        Wait-Job $Inventory
+        $r = Receive-Job $Inventory
+        $r | ForEach-Object {$_ > $env:ProgramFiles\blue\windows\logs\$($_.PSComputerName).trlout}
+    
+        Read-Host -Prompt "Continue with Hardening? Press Enter to continue."
+        $Hardening = Invoke-Command -FilePath $env:ProgramFiles\blue\windows\Invoke-SecureBaseline.ps1 -Session $Session -AsJob
+        Wait-Job $Hardening
+        $r = Receive-Job $Hardening
+        $r | ForEach-Object {$_ > $env:ProgramFiles\blue\windows\logs\$($_.PSComputerName).secbout}
+    } 
 }
