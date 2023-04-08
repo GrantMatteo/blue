a46045d45615681bb141c4d1c789c04c8a443fed
justin <jcovairt01@gmail.com>
2023-04-08 13:53:17 -0700

    version update + fixes

--- Threat Hunting/Vuln Scanning/scans.sh
+++ Threat Hunting/Vuln Scanning/scans.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-apt-get update -y && apt install nuclei unzip nmap chromium -y 
+apt-get update -y && apt-get install unzip nmap chromium golang -y 
+wget -O /root/nuclei.zip https://github.com/projectdiscovery/nuclei/releases/download/v2.9.1/nuclei_2.9.1_linux_amd64.zip
+unzip /root/nuclei.zip
+mv nuclei /usr/bin/nuclei
 nuclei -i $1 -me /root/initial
 wget http://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
-apt-get install unzip
 unzip aquatone_linux_amd64_1.7.0.zip
 mv aquatone /usr/bin
 nmap -T5 -oX /root/foraqua $1 
--- Threat Hunting/manager-install.sh
+++ Threat Hunting/manager-install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-curl -sO https://packages.wazuh.com/4.3/wazuh-install.sh && bash ./wazuh-install.sh -a -i
+curl -sO https://packages.wazuh.com/4.4/wazuh-install.sh && bash ./wazuh-install.sh -a -i
 /usr/share/wazuh-indexer/plugins/opensearch-security/tools/wazuh-passwords-tool.sh  -u admin -p 'NotTheRealPassword123?'
 
 echo "
 <agent_config>
 
-    <localfile>
-        <location>Microsoft-Windows-Sysmon/Operational</location>
-        <log_format>eventchannel</log_format>
-    </localfile>
+        <localfile>
+                <location>Microsoft-Windows-Sysmon/Operational</location>
+                <log_format>eventchannel</log_format>
+        </localfile>
 
-    <localfile>
-        <location>Microsoft-Windows-Windows Defender /Operational</location>
-        <log_format>eventchannel</log_format>
-    </localfile>
+        <localfile>
+                <location>Microsoft-Windows-Windows Defender /Operational</location>
+                <log_format>eventchannel</log_format>
+        </localfile>
 
-    <localfile>
-        <location>Security</location>
-        <log_format>eventchannel</log_format>
-    </localfile>
+        <localfile>
+                <location>Security</location>
+                <log_format>eventchannel</log_format>
+        </localfile>
 
-    <localfile>
-        <location>System</location>
-        <log_format>eventchannel</log_format>
-    </localfile>
+        <localfile>
+                <location>System</location>
+                <log_format>eventchannel</log_format>
+        </localfile>
 
         <localfile>
                 <log_format>syslog</log_format>
@@ -60,9 +60,7 @@ echo "
                 <directories check_all="yes" whodata="yes">/var/www</directories>
                 <directories check_all="yes" whodata="yes">/root</directories>
                 <directories check_all="yes" whodata="yes">/tmp</directories>
-                <directories check_all="yes" whodata="yes">C:/Windows/Tasks</directories>
                 <directories check_all="yes" whodata="yes">C:/Users/*/Appdata</directories>
-                <directories check_all="yes" whodata="yes">C:\Windows\System32\config\systemprofile\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine</directories> 
                 <windows_registry arch="both" check_all="yes">HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule</windows_registry>
                 <windows_registry arch="both" check_all"yes">HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree</windows_registry>
                 <windows_registry arch="both" check_all="yes">HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W3SVC</windows_registry>
@@ -75,8 +73,8 @@ echo "
 
 curl -so ~/wazuh_socfortress_rules.sh https://raw.githubusercontent.com/socfortress/Wazuh-Rules/main/wazuh_socfortress_rules.sh && bash ~/wazuh_socfortress_rules.sh
 
-wget https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10-1.msi -O /root/wazuh-agent.msi
-curl -so /tmp/wazuh-agent.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.3.10-1_amd64.deb
-curl -so /tmp/wazuh-agent.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.3.10-1.x86_64.rpm
+curl -so /root/wazuh-agent.msi https://packages.wazuh.com/4.x/windows/wazuh-agent-4.4.0-1.msi 
+curl -so /root/wazuh-agent.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.4.0-1_amd64.deb
+curl -so /root/wazuh-agent.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.4.0-1.x86_64.rpm
 
 systemctl restart wazuh-manager
