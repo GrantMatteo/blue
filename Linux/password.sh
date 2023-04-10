c2a4ade7511054c8fefcd61cfaa3ab1ace9c5fad
susMdT <68256613+susMdT@users.noreply.github.com>
2023-04-09 20:27:35 -0700

    Update password.sh

--- Linux/password.sh
+++ Linux/password.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+if [ -z "$ROOTPASS" ]; then
+    echo "One or more variables are empty. Exiting to prevent lockout."
+    exit 1
+fi
 echo "username,password"
 for user in $(cat /etc/passwd | grep -E "/bin/.*sh" | cut -f1 -d':'); do
         pass=$(cat /dev/urandom | tr -dc '[:alpha:][:digit:]' | fold -w ${1:-20} | head -n 1)
