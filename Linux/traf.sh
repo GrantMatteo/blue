a628fb124db63d6b159d883219cf04e641e0e082
susMdT <68256613+susMdT@users.noreply.github.com>
2023-04-10 11:48:32 -0700

    Update traf.sh

--- Linux/traf.sh
+++ Linux/traf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /var/log/iptra*/*.log | awk -F ';' '{print $5}' | awk '{print $2}' | sort -r | uniq -c | sort -r
+cat /var/log/iptra*/*.log | awk -F ';' '{print $5}' | awk '{print $2}' | sort | uniq -c | sort -rn | head -n 10
