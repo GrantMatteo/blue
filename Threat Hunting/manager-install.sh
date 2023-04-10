5c420ffff7a1863655f5f0b41ea1f997dfb29831
Justin <jcovairt01@gmail.com>
2023-04-10 08:43:59 -0700

    S1E11

--- Threat Hunting/manager-install.sh
+++ Threat Hunting/manager-install.sh
@@ -3,6 +3,9 @@
 curl -sO https://packages.wazuh.com/4.4/wazuh-install.sh && bash ./wazuh-install.sh -a -i
 /usr/share/wazuh-indexer/plugins/opensearch-security/tools/wazuh-passwords-tool.sh  -u admin -p 'NotTheRealPassword123?'
 
+mkdir /var/ossec/etc/shared/{windows,linux}
+cp -r /var/ossec/etc/shared/default/*.txt /var/ossec/etc/shared/windows/ && cp -r /var/ossec/etc/shared/default/*.txt /var/ossec/etc/shared/linux/
+chown -R wazuh:wazuh /var/ossec/etc/shared/{windows,linux}
 echo "
 <agent_config>
 
@@ -26,6 +29,19 @@ echo "
                 <log_format>eventchannel</log_format>
         </localfile>
 
+        <syscheck>
+                <directories check_all="yes" whodata="yes">C:/Users/*/Appdata</directories>
+                <windows_registry arch="both" check_all="yes">HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule</windows_registry>
+                <windows_registry arch="both" check_all"yes">HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree</windows_registry>
+                <windows_registry arch="both" check_all="yes">HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W3SVC</windows_registry>
+                <windows_registry arch="both" check_all"yes">HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DNS</windows_registry>
+        </syscheck>
+
+</agent_config>
+" > /var/ossec/shared/windows/agent.conf
+
+echo "
+<agent_config>
         <localfile>
                 <log_format>syslog</log_format>
                 <location>/var/log/auth.log</location>
@@ -59,16 +75,10 @@ echo "
                 <directories check_all="yes" whodata="yes">/etc</directories> 
                 <directories check_all="yes" whodata="yes">/var/www</directories>
                 <directories check_all="yes" whodata="yes">/root</directories>
-                <directories check_all="yes" whodata="yes">/tmp</directories>
-                <directories check_all="yes" whodata="yes">C:/Users/*/Appdata</directories>
-                <windows_registry arch="both" check_all="yes">HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule</windows_registry>
-                <windows_registry arch="both" check_all"yes">HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree</windows_registry>
-                <windows_registry arch="both" check_all="yes">HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W3SVC</windows_registry>
-                <windows_registry arch="both" check_all"yes">HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DNS</windows_registry>
-               
+                <directories check_all="yes" whodata="yes">/tmp</directories>   
         </syscheck>
 </agent_config>
-" > /var/ossec/etc/shared/default/agent.conf
+" > /var/ossec/etc/shared/linux/agent.conf
 
 
 curl -so ~/wazuh_socfortress_rules.sh https://raw.githubusercontent.com/socfortress/Wazuh-Rules/main/wazuh_socfortress_rules.sh && bash ~/wazuh_socfortress_rules.sh
