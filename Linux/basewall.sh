207ed372007975f7871a23580e0811a0c315f8f5
susMdT <68256613+susMdT@users.noreply.github.com>
2023-04-09 21:09:35 -0700

    Update basewall.sh

--- Linux/basewall.sh
+++ Linux/basewall.sh
@@ -7,15 +7,25 @@ ipt=$(command -v iptables || command -v /sbin/iptables || command -v /usr/sbin/i
 #LOCALNETWORK = Subnet(s) of machines that depend on us and vice versa
 
 if [ -z "$LOCALNETWORK" ]; then
-    echo "One or more variables are empty. Exiting to prevent lockout."
+    echo "LOCALNETWORK not defined."
+    exit 1
+fi
+
+if [ -z "$CCSHOST" ]; then
+    echo "CCSHOST not defined."
     exit 1
 fi
 
 # Flush the current rules
 $ipt -P OUTPUT ACCEPT ; $ipt -P FORWARD ACCEPT ; $ipt -F; $ipt -X ;$ipt -P INPUT ACCEPT 
 
-# Allow our machine to respond to connections
+# Allow our machine to respond to connections (INPUT is for when we inevitably write manual rules to prevent outbound hangs)
+$ipt -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 $ipt -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# CCS ENDPOINT
+$ipt -A OUTPUT -d $CCHOST -j ACCEPT
+$ipt -A INPUT -d $CCHOST -j ACCEPT
 
 # Allow outbound connetions to dependencies we need from other machines + outbound local network DNS
 $ipt -A OUTPUT -d 127.0.0.1,$LOCALNETWORK -m conntrack --ctstate NEW -j ACCEPT
